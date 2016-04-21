7383fc47ee0ee9d5571a7b7662fa9e034d06da70
pdigiglio <p.digiglio91@gmail.com>
2016-04-21 14:28:41 +0200

    edit mkentry.sh

--- mkentry.sh
+++ mkentry.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-echo "\\newglossaryentry{$1}{%"
-echo "    name        = {$1},%"
-echo "    description = {}%"
-echo "}"
+record () {
+	local dbPath="listeParole/"
+	echo " > Recording `tput setaf 3`$1`tput sgr0` into " \
+		"`tput setaf 2`${dbPath}`echo ${1:0:1} | tr '[:lower:]' '[:upper:]'`.tex `tput sgr0`" > /dev/stderr
+
+	echo "\\newglossaryentry{$1}{%"
+	echo "    name        = {$1},%"
+	echo "    description = {}%"
+	echo "}"
+}
+
+record $1
