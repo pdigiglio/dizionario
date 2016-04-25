b5845f7c9f53ce7fe72c650356c981676b6b67eb
pdigiglio <p.digiglio91@gmail.com>
2016-04-25 11:58:52 +0200

    update mkhisto.sh

--- mkhisto.sh
+++ mkhisto.sh
@@ -3,8 +3,19 @@
 # Import some variables
 . commonVariables.sh
 
+histoDataFile="histoData.dat"
+
 extract_histo_data () {
-    grep "newglossaryentry" ${dbPath}/*.tex | cut -f2 -d% | sed 's/^\ //' # | sort --numeric-sort
+	# Check if output is forced
+	if [[ -z "$1" ]]
+	then
+		output="$histoDataFile"
+	else
+		output="$1"
+	fi
+
+	# Extract relevant data and override previous data file (if any)
+    grep "newglossaryentry" ${dbPath}/*.tex | cut -f2 -d% | sed 's/^\ //' > $output # | sort --numeric-sort
 }
 
-extract_histo_data
+extract_histo_data "$1"
