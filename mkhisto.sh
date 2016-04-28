65305c483f5e5fa11f397cee215481622fee1748
pdigiglio <p.digiglio91@gmail.com>
2016-04-28 14:55:09 +0200

    add cmd-line options

--- mkhisto.sh
+++ mkhisto.sh
@@ -4,6 +4,7 @@
 . commonVariables.sh
 
 histoDataFile="histoData.dat"
+histoScript="mkHistogram.gpl"
 
 extract_histo_data () {
 	# Check if output is forced
@@ -18,4 +19,58 @@ extract_histo_data () {
     grep "newglossaryentry" ${dbPath}/*.tex | cut -f2 -d% | sed 's/^\ //' > $output # | sort --numeric-sort
 }
 
-extract_histo_data "$1"
+while [[ $# -ge 1 ]]
+do
+	key="$1"
+	case $key in 
+		--to-stdout)
+			extract_histo_data "/dev/stdout"
+#			shift
+			;;
+
+		--to-stderr)
+			extract_histo_data "/dev/stderr"
+#			shift
+			;;
+
+		-c|--count)
+			extract_histo_data "/dev/stdout" | wc -l
+#			shift
+			;;
+
+		-o|--output)
+			if [[ !( -z "$2" ) ]]
+			then
+				extract_histo_data "$2"
+				shift
+			else
+				echo "Empty file name. Exiting." > /dev/stderr
+				exit 1
+			fi
+			;;
+
+		--get-default-output)
+			echo `pwd`"$histoDataFile"
+#			shift
+			;;
+
+		-h|--help)
+			echo "TODO: usage"
+#			shift
+			;;
+
+		--make)
+			extract_histo_data "$histoDataFile"&& gnuplot $histoScript #&& xdg-open `echo $histoDataFile| sed 's/.dat/.png/'`&
+#			shift
+			;;
+		--visualize)
+			extract_histo_data "$histoDataFile"&& gnuplot $histoScript&& xdg-open `echo $histoDataFile| sed 's/.dat/.png/'`&
+#			shift
+			;;
+			
+		*)
+			extract_histo_data "$histoDataFile"
+			;;
+	esac
+	shift
+done
